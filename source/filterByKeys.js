1a22223a05cc61ffd89c9cb5ac75f5f2eca246cc
Rav0 <ivateem@gmail.com>
2026-09-13 14:04:31 +0300

    docs: добавил примеры использования функции filterObjectByKeys.

--- source/filterByKeys.js
+++ source/filterByKeys.js
@@ -6,22 +6,37 @@
  * Копирование полей происходит с помощью structuredClone.
  * Если поле невозможно скопировать, сохраняется ссылка на этот объект.
  * При несоответствии типов входных данных, вернется пустой объект {}.
+ * Функция не изменяет входные данные.
  * @param {Object} obj - объект
  * @param {Array<String | Number>} keys - список ключей (полей).
  * @returns {Object} объект, содержащий только поля из keys
  * 
- * @example
+ * @example Основное поведение
  * const obj = {
- *  "key1": 1,
- *  "key2": 2,
- *  "key3": 3,
- *  "key4": 4,
- *  "key5": 5,
- *  "key_string": "any_string"
- *  };
- *  const filteredObj = filterObjectByKeys(obj, ["key1", "key_string", "key3", "not_exists_key"]);
+ *      key1: 1,
+ *      key2: 2,
+ *      key3: 3,
+ *      key4: 4,
+ *      key5: 5,
+ *      key_string: "any_string"
+ * };
+ * const filteredObj = filterObjectByKeys(obj, ["key1", "key_string", "key3", "not_exists_key"]);
  *
- *  // filteredObj = {"key1": 1, "key_string": "any_string", "key3": 3}
+ * // filteredObj = {"key1": 1, "key_string": "any_string", "key3": 3}
+ * 
+ * @example Поведение при некопируемом объекте
+ * const echo = (text) => console.log(text);
+ * const obj = {
+ *      f: echo,
+ *      key: "any" 
+ * };
+ * 
+ * const filteredObj = filterObjectByKeys(obj, ["f"]);
+ * // filteredObj = {f: echo};
+ * 
+ * @example Поведение, если аргументы не соответствуют типам
+ * const filteredObj = filterObjectByKeys("STRING", 123456);
+ * // filteredObj = {};
 */
 function filterObjectByKeys(obj, keys) {
     if (typeof obj !== "object") {
